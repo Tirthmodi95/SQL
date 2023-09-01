0730467ea226d660d8ce4d04373bc897f908d9ba
Tirth Modi <143075272+Tirthmodi95@users.noreply.github.com>
2023-09-01 13:15:36 +0530

    Add files via upload

---
+++ MySql_Extract File For Sales_data_final.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE SALES
+USE SALES
+CREATE TABLE SALES1(
+	order_id VARCHAR(15) NOT NULL, 
+	order_date VARCHAR(15) NOT NULL, 
+	ship_date VARCHAR(15) NOT NULL, 
+	ship_mode VARCHAR(14) NOT NULL, 
+	customer_name VARCHAR(22) NOT NULL, 
+	segment VARCHAR(11) NOT NULL, 
+	state VARCHAR(36) NOT NULL, 
+	country VARCHAR(32) NOT NULL, 
+	market VARCHAR(6) NOT NULL, 
+	region VARCHAR(14) NOT NULL, 
+	product_id VARCHAR(16) NOT NULL, 
+	category VARCHAR(15) NOT NULL, 
+	sub_category VARCHAR(11) NOT NULL, 
+	product_name VARCHAR(127) NOT NULL, 
+	sales DECIMAL(38, 0) NOT NULL, 
+	quantity DECIMAL(38, 0) NOT NULL, 
+	discount DECIMAL(38, 3) NOT NULL, 
+	profit DECIMAL(38, 8) NOT NULL, 
+	shipping_cost DECIMAL(38, 2) NOT NULL, 
+	order_priority VARCHAR(8) NOT NULL, 
+	`year` DECIMAL(38, 0) NOT NULL
+);
+SET SESSION sql_mode = ''
+LOAD DATA INFILE 
+'E:/sales_data_final.csv'
+into table sales1 
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 rows 
+SELECT * FROM SALES1
